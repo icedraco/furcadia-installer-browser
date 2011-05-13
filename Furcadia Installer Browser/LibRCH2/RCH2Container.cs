89a2c5a7ba74eb43f35d97fd02ad5cb788d1f30b
IceDragon <icedragon@quickfox.org>
2011-05-13 02:26:51 +0000

    Initial import

---
+++ Furcadia Installer Browser/LibRCH2/RCH2Container.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace LibRCH2
+{
+    /// <summary>
+    /// This class represents an RCH2.0 "container", which is a single file
+    /// that contains other files within it.
+    /// </summary>
+    public class RCH2Container
+    {
+        /*** Constants ***/
+        public const string MAGIC_NUMBER = "RCH2.0";
+
+        /*** Data Members ***/
+        private BinaryReader br = null;
+        public List<RCH2File> Files = new List<RCH2File>();
+       
+        /*** Properties ***/
+        public int Count { get { return Files.Count; } }
+
+        /*** Static Functions ***/
+        public static RCH2Container CreateFromReader(ref BinaryReader br)
+        {
+            return new RCH2Container(ref br);
+        }
+
+        /// <summary>
+        /// Checks whether the magic number readen from an open binary reader
+        /// instance is correct or not.
+        /// 
+        /// NOTE: The binary reader is not reset to its previous position after
+        ///       the check!
+        /// </summary>
+        /// <param name="br"></param>
+        /// <returns></returns>
+        public static bool CheckMagicNumber(ref BinaryReader br)
+        {
+            byte[] magic = br.ReadBytes(MAGIC_NUMBER.Length);
+            return Encoding.ASCII.GetString(magic) == MAGIC_NUMBER;
+        }
+
+        /*** Constructor ***/
+        public RCH2Container()
+        {
+        }
+
+        /// <summary>
+        /// Initialize a new RCH2 container instance from an already open
+        /// BinaryReader.
+        /// 
+        /// The initialization begins from the current position of the BinaryReader
+        /// and not from the beginning of the file!
+        /// 
+        /// The reader doesn't get closed at the end of the process, nor does the
+        /// position get reset to what it was!
+        /// </summary>
+        /// <param name="br">An open binary reader to read from</param>
+        public RCH2Container(ref BinaryReader br)
+        {
+            SetBinaryReader(ref br);
+            ProcessData();
+        }
+
+        /// <summary>
+        /// Initialize a new RCH2 container instance from a specific filename.
+        /// </summary>
+        /// <param name="fileName">Filename to read data from</param>
+        public RCH2Container(string fileName)
+        {
+            Open(fileName);
+        }
+
+        /*** Methods ***/
+        public void SetBinaryReader(ref BinaryReader br)
+        {
+            this.br = br;
+        }
+
+        /// <summary>
+        /// Opens a file and initializes the object from within it.
+        /// 
+        /// An exception is thrown if something goes wrong.
+        /// </summary>
+        /// <param name="fileName">Filename to read data from</param>
+        public void Open(string fileName)
+        {
+            br = new BinaryReader(new FileStream(fileName, FileMode.Open, FileAccess.Read));
+            ProcessData();
+        }
+
+        /// <summary>
+        /// Begins processing data from a currently open BinaryReader, be it
+        /// from the Open() method, or SetBinaryReader().
+        /// 
+        /// An exception is thrown if something goes wrong.
+        /// </summary>
+        public void ProcessData()
+        {
+            // Clear the file list since we're starting afresh here.
+            Files.Clear();
+
+            // Check magic number.
+            if (!CheckMagicNumber(ref br))
+                throw new Exception("Not an RCH2.0 file or bad magic number");
+
+            // Start creating file entries from the rest.
+            RCH2File file;
+            while ((file = RCH2File.CreateFromReader(ref br)) != null)
+                Files.Add(file);
+        }
+    }
+}
---
+++ Furcadia Installer Browser/RCH2Compare.cs
@@ -0,0 +1,140 @@
+﻿/*** RCH2Compare Class
+ * (c) 2010 by IceDragon of QuickFox.org
+ * 
+ * <icedragon@quickfox.org>
+ * http://www.icerealm.org/
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace LibRCH2
+{
+    /// <summary>
+    /// This class is responsible for comparing two RCH2Container objects and
+    /// determining if they are identical, which files are different between
+    /// the two, and which files are unique to each container.
+    /// </summary>
+    public class RCH2Compare
+    {
+        /*** Data Members ***/
+        public List<RCH2File> IdenticalFiles = new List<RCH2File>();
+        public CompareFile File1;
+        public CompareFile File2;
+
+        /*** Properties ***/
+        /// <summary>
+        /// TRUE if two install packages are considered identical.
+        /// </summary>
+        public bool Identical
+        {
+            get {
+                if (compared)
+                    return (File1.DifferentFiles.Count +
+                    File1.UniqueFiles.Count +
+                    File2.DifferentFiles.Count +
+                    File2.UniqueFiles.Count) == 0;
+                else
+                    return Compare();
+            }
+        }
+        public RCH2Container SourceContainer1
+        {
+            get { return File1.Container; }
+            set { File1 = new CompareFile(ref value); }
+        }
+        public RCH2Container SourceContainer2
+        {
+            get { return File2.Container; }
+            set { File2 = new CompareFile(ref value); }
+        }
+
+        /*** Constructor ***/
+        public RCH2Compare(ref RCH2Container file1, ref RCH2Container file2) : this(ref file1, ref file2, true) { }
+        public RCH2Compare(ref RCH2Container file1, ref RCH2Container file2, bool doCompare)
+        {
+            SourceContainer1 = file1;
+            SourceContainer2 = file2;
+
+            if (doCompare)
+                Compare();
+        }
+
+        /*** Methods ***/
+        /// <summary>
+        /// Performs a comparison between the two installer packages and returns
+        /// if the two of them carry the same content within.
+        /// 
+        /// Information on what's different and what's the same is stored within
+        /// the public data members.
+        /// </summary>
+        /// <returns>TRUE if both install packages carry the same data. FALSE otherwise.</returns>
+        public bool Compare()
+        {
+            // If one of the containers is null, we have a problem.
+            if (File1.Container == null || File2.Container == null)
+                throw new Exception("Attempted to compare with a null RCH2 container");
+
+            // Clear the files in case and something's already inside.
+            File1.DifferentFiles.Clear();
+            File1.UniqueFiles.Clear();
+            File2.DifferentFiles.Clear();
+            File2.UniqueFiles.Clear();
+            IdenticalFiles.Clear();
+
+            // Rearrange the second container into a dictionary for quick access.
+            Dictionary<string, RCH2File> file2_list = new Dictionary<string, RCH2File>(File2.Container.Count);
+
+            foreach (RCH2File file in File2.Container.Files)
+                file2_list[file.Path] = file;
+
+            // Compare the two lists.
+            foreach (RCH2File file in File1.Container.Files)
+            {
+                RCH2File file2;
+                if (file2_list.TryGetValue(file.Path, out file2))
+                {
+                    // Compare the two files.
+                    if (file.CRCSum != file2.CRCSum)
+                    {
+                        File1.DifferentFiles.Add(file);
+                        File2.DifferentFiles.Add(file2);
+                    }
+                    else
+                        IdenticalFiles.Add(file);
+
+                    // Remove this path from the second list so what's left is
+                    // the unique ones.
+                    file2_list.Remove(file.Path);
+                }
+                else
+                    File1.UniqueFiles.Add(file);
+            }
+
+            foreach (RCH2File file in file2_list.Values)
+                File2.UniqueFiles.Add(file);
+
+            // Return if the two files are identical.
+            compared = true;
+            return Identical;
+        }
+
+        #region Internal Stuff
+        public struct CompareFile
+        {
+            public RCH2Container Container;
+            public List<RCH2File> DifferentFiles;
+            public List<RCH2File> UniqueFiles;
+
+            public CompareFile(ref RCH2Container rch2)
+            {
+                Container = rch2;
+                DifferentFiles = new List<RCH2File>();
+                UniqueFiles = new List<RCH2File>();
+            }
+        };
+
+        private bool compared = false;
+        #endregion
+    }
+}
